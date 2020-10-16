c56585872d0510e491fd6e07fec96fb38cb07369
Jeremy Andrews <jeremy@tag1consulting.com>
2020-10-16 12:56:22 +0200

    deduplicate

--- src/common.rs
+++ src/common.rs
@@ -2,6 +2,7 @@ use goose::goose::GooseResponse;
 use goose::prelude::*;
 
 use regex::Regex;
+use log::info;
 
 /// The Umami website defines three content types.
 pub enum ContentType {
@@ -255,4 +256,135 @@ pub fn get_form_value(html: &str, name: &str) -> Option<String> {
         Some(value) => Some(value[1].to_string()),
         None => None,
     }
+}
+
+/// Anonymously load the contact form and POST feedback. The english boolean flag indicates
+/// whether to load the English form or the Spanish form.
+pub async fn anonymous_contact_form(user: &GooseUser, english: bool) -> GooseTaskResult {
+    let contact_form_url = if english {
+        "/en/contact"
+    } else {
+        "/es/contact"
+    };
+    let mut goose = user.get(contact_form_url).await?;
+
+    // We can't invoke common::validate_and_load_static_assets as while it's important
+    // to validate the page and load static elements, we then need to extra form elements
+    // from the HTML of the page. So we duplicate some of the logic, enhancing it for form
+    // processing.
+    let contact_form;
+    match goose.response {
+        Ok(response) => {
+            // Copy the headers so we have them for logging if there are errors.
+            let headers = &response.headers().clone();
+            match response.text().await {
+                Ok(html) => {
+                    // Be sure we've properly loaded the Contact form.
+                    let title = if english {
+                        "Website feedback"
+                    } else {
+                        "Comentarios sobre el sitio web"
+                    };
+                    if !valid_title(&html, title) {
+                        return user.set_failure(
+                            &format!("{}: title not found: {}", goose.request.url, title),
+                            &mut goose.request,
+                            Some(&headers),
+                            Some(&html),
+                        );
+                    }
+
+                    // Load all static elements on the page, as a real user would.
+                    load_static_elements(user, &html).await;
+
+                    // Scrape the HTML to get the values needed in order to POST to the
+                    // contact form.
+                    let form_build_id = get_form_value(&html, "form_build_id");
+                    if form_build_id.is_none() {
+                        return user.set_failure(
+                            &format!("{}: no form_build_id on page", goose.request.url),
+                            &mut goose.request,
+                            Some(&headers),
+                            Some(&html),
+                        );
+                    }
+
+                    // Build contact form parameters.
+                    // @TODO: dynamically generate the content.
+                    let params = [
+                        ("name", "@TODO: name"),
+                        ("mail", "nobody@example.com"),
+                        ("subject[0][value]", "@TODO: subject"),
+                        ("message[0][value]", "@TODO: message"),
+                        ("form_build_id", &form_build_id.unwrap()),
+                        ("form_id", "contact_message_feedback_form"),
+                        ("op", "Send+message"),
+                    ];
+                    let request_builder = user.goose_post(contact_form_url).await?;
+                    contact_form = user.goose_send(request_builder.form(&params), None).await?;
+                }
+                Err(e) => {
+                    return user.set_failure(
+                        &format!("{}: failed to parse page: {}", goose.request.url, e),
+                        &mut goose.request,
+                        Some(&headers),
+                        None,
+                    );
+                }
+            }
+        }
+        Err(e) => {
+            return user.set_failure(
+                &format!("{}: no response from server: {}", goose.request.url, e),
+                &mut goose.request,
+                None,
+                None,
+            );
+        }
+    }
+
+    // Drupal 9 throttles how many times an IP address can submit the contact form, so we
+    // need special handling. We check the response, and issue an info! level message if
+    // the form is throttled. This is a valid event to load test.
+    match contact_form.response {
+        Ok(response) => {
+            // Copy the headers so we have them for logging if there are errors.
+            let headers = &response.headers().clone();
+            match response.text().await {
+                Ok(html) => {
+                    // If the contact form succeeded, we were redirected to the home page.
+                    let error_text = if english {
+                        "You cannot send more than"
+                    } else {
+                        "No le está permitido enviar más"
+                    };
+                    if html.contains(error_text) {
+                        info!("post to contact form was throttled: {}", contact_form.request.url);
+                    }
+
+                    // Either way, a "real" user would still load all static elements on
+                    // the returned page.
+                    load_static_elements(user, &html).await;
+                },
+                Err(e) => {
+                    return user.set_failure(
+                        &format!("{}: failed to parse page: {}", goose.request.url, e),
+                        &mut goose.request,
+                        Some(&headers),
+                        None,
+                    );
+                }
+            }
+        }
+        Err(e) => {
+            return user.set_failure(
+                &format!("{}: no response from server: {}", goose.request.url, e),
+                &mut goose.request,
+                None,
+                None,
+            );
+        }
+    }
+
+    Ok(())
 }
